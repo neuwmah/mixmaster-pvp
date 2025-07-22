52a5ba152831dd1e4251293ca232c84b366988ce
Guilherme Neumann <ae81ff@gmail.com>
2025-07-22 08:37:44 -0300

    Update actions hover

--- src/app/components/header/Actions.tsx
+++ src/app/components/header/Actions.tsx
@@ -16,19 +16,19 @@ const Actions: React.FC<{ openMenuMobile?: () => void }> = ({ openMenuMobile }) 
 
   return (
     <div className="actions flex items-center gap-8">
-      <Link className="link flex text-white duration-250 hover:text-(--personal)" href="/download">
+      <Link className="link flex text-white duration-250 hover:text-(--primary-orange-1)" href="/download">
         <ArrowDownTrayIcon className="icon" />
       </Link>
       {mobile 
         ? <button
-          className="button cursor-pointer text-white duration-250 hover:text-(--personal)"
+          className="button cursor-pointer text-white duration-250 hover:text-(--primary-orange-1)"
           type="button"
           onClick={openMenuMobile}
         >
           <Bars3Icon className="icon" />
         </button>
         : <Link 
-          className="link flex text-white duration-250 hover:text-(--personal)" 
+          className="link flex text-white duration-250 hover:text-(--primary-orange-1)" 
           href="/account"
         >
           <UserIcon className="icon" />
