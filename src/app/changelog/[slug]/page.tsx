59ade9195d9089375ec4c23000b2ca66864295be
Guilherme Neumann <ae81ff@gmail.com>
2025-07-22 08:37:57 -0300

    Update post not found

--- src/app/changelog/[slug]/page.tsx
+++ src/app/changelog/[slug]/page.tsx
@@ -8,26 +8,37 @@ export default async function ChangelogPage({ params }: Props) {
   const { slug } = await params;
 
   const post = changelogData.find((p) => p.slug === slug);
-
-  if (!post) {
-    return <div className="container">Post not found.</div>;
-  }
-
+  
   return (
     <main>
-      <section className="section-changelog section">
-        <div className="container flex justify-center">
-          <div className="content flex flex-col items-center w-full max-w-[700px]">
+      {post 
+        ? (
+          <section className="section-changelog section">
+            <div className="container flex justify-center">
+              <div className="content flex flex-col items-center w-full max-w-[700px]">
 
-            <h1 className="title text-center">{post.title}</h1>
+                <h1 className="title text-center">{post.title}</h1>
 
-            <p className="text-xs text-center mt-2 text-(--gray-4)">{post.date}</p>
+                <p className="text-xs text-center mt-2 text-(--gray-4)">{post.date}</p>
 
-            <div className="text-base text-center text-white mt-12">{post.content}</div>
+                <div className="text-base text-center text-white mt-12">{post.content}</div>
 
+              </div>
+            </div>
+          </section>
+        )
+        : (
+          <div className="section-not-found section">
+            <div className="container flex flex-col items-center">
+
+              <h1 className="title text-center">❗ 404</h1>
+
+              <p className="text-base text-center mt-6 text-(--gray-4)">Not found.</p>
+
+            </div>
           </div>
-        </div>
-      </section>
+        )
+      }
     </main>
   );
 }
