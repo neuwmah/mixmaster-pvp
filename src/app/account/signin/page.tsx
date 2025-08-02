052856fa8999bdc7bb29ba86fb04293a3c333ca5
Guilherme Neumann <ae81ff@gmail.com>
2025-08-02 18:29:21 -0300

    Add main sections

--- src/app/account/signin/page.tsx
+++ src/app/account/signin/page.tsx
@@ -5,20 +5,22 @@ import Form from './Form';
 
 export default async function SignInPage() {
   return (
-    <section className="section-signup section">
-      <div className="container flex-col items-center">
+    <main>
+      <section className="section-signup section">
+        <div className="container flex-col items-center">
 
-        <h1 className="title">
-          Sign In
-        </h1>
+          <h1 className="title">
+            Sign In
+          </h1>
 
-        <p className="text-base mt-6">
-          New account? <Link className="link text-base duration-250 text-(--primary-orange-1) hover:underline" href="/account/signup">Click here</Link>
-        </p>
+          <p className="text-base mt-6">
+            New account? <Link className="link text-base duration-250 text-(--primary-orange-1) hover:underline" href="/account/signup">Click here</Link>
+          </p>
 
-        <Form />
+          <Form />
 
-      </div>
-    </section>
+        </div>
+      </section>
+    </main>
   );
 }
--- src/app/account/signup/page.tsx
+++ src/app/account/signup/page.tsx
@@ -5,20 +5,22 @@ import Form from './Form';
 
 export default async function SignUpPage() {
   return (
-    <section className="section-signup section">
-      <div className="container flex-col items-center">
+    <main>
+      <section className="section-signup section">
+        <div className="container flex-col items-center">
 
-        <h1 className="title">
-          Sign Up
-        </h1>
-        
-        <p className="text-base mt-6">
-          Already have an account? <Link className="link text-base duration-250 text-(--primary-orange-1) hover:underline" href="/account/signin">Click here</Link>
-        </p>
-        
-        <Form />
+          <h1 className="title">
+            Sign Up
+          </h1>
+          
+          <p className="text-base mt-6">
+            Already have an account? <Link className="link text-base duration-250 text-(--primary-orange-1) hover:underline" href="/account/signin">Click here</Link>
+          </p>
+          
+          <Form />
 
-      </div>
-    </section>
+        </div>
+      </section>
+    </main>
   )
 }
