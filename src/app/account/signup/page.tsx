f6ac650dd01f37800410523f1f7d349f852f6600
Guilherme Neumann <ae81ff@gmail.com>
2025-08-02 22:22:55 -0300

    Update user token auth

--- src/app/account/signup/page.tsx
+++ src/app/account/signup/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
 
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+
 import Form from './Form';
 
 export default async function SignUpPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('sessionToken')?.value;
+
+  if (token)
+    return redirect('/account')
+
   return (
     <main className="flex flex-1 bg-gradient-to-b from-black to-(--gray-0) sm:items-center">
       <section className="section-signup section">
