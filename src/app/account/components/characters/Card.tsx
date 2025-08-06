f6b8255a448b8a895e4a07ba954fdc2080af65d6
Guilherme Neumann <ae81ff@gmail.com>
2025-08-06 06:03:20 -0300

    Add new character card states

--- src/app/account/components/characters/Card.tsx
+++ src/app/account/components/characters/Card.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import {
   ArrowUturnLeftIcon,
   DocumentArrowUpIcon,
+  MapPinIcon,
   PencilIcon,
   PencilSquareIcon
 } from '@heroicons/react/24/outline';
@@ -12,9 +13,12 @@ import { Character } from '@/types/character';
 
 export default function Card(character: Character) {
   const [edit, setEdit] = useState(false);
+  const [changeNickname, setChangeNickname] = useState(false)
+  const [transferCharacter, setTransferCharacter] = useState(false)
+  const [resetPosition, setResetPosition] = useState(false)
   
   return (
-    <div key={character.id} className="info text-ellipsis overflow-hidden min-w-0 p-8 bg-(--black) relative">
+    <div key={character.id} className="info text-ellipsis overflow-hidden min-w-0 p-8 bg-(--black) relative min-h-[24rem]">
       <img 
         className="absolute top-0 left-0 object-cover w-full h-full opacity-[.7] filter-[brightness(.2)]"
         src={`/assets/images/characters/${character.class.toLocaleLowerCase()}.jpg`}
@@ -22,37 +26,87 @@ export default function Card(character: Character) {
       />
       <div className="relative z-1">
         {edit
-          ? <>
-            <p className="text-sm">Name: <strong>{character.name}</strong></p>
-            <button
-              className="flex items-center gap-4 mt-2 pointer-events-auto cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
-              type="button"
-            >
-              <PencilIcon className="icon max-w-6 max-h-6" /> Change nickname
-            </button>
-            <p className="text-sm mt-6">User: <strong>{character.user.id}</strong></p>
-            <button
-              className="flex items-center gap-4 mt-2 pointer-events-auto cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
-              type="button"
-            >
-              <DocumentArrowUpIcon className="icon max-w-6 max-h-6" /> Transfer character
-            </button>
-          </>
+          ? (changeNickname 
+            ? <>
+              <button
+                className="cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
+                type="button"
+                onClick={() => { setChangeNickname(!changeNickname) }}
+              >
+                Return
+              </button>
+              <p className="text-sm mt-8">
+                New name
+              </p>
+            </>
+            : transferCharacter
+              ? <>
+                <button
+                  className="cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
+                  type="button"
+                  onClick={() => { setTransferCharacter(!transferCharacter) }}
+                >
+                  Return
+                </button>
+                <p className="text-sm mt-8">
+                  New user ID
+                </p>
+              </>
+              : resetPosition
+                ? <>
+                  <button
+                    className="cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
+                    type="button"
+                    onClick={() => { setResetPosition(!resetPosition) }}
+                  >
+                    Return
+                  </button>
+                  <p className="text-sm mt-8">
+                    Select a city to transfer
+                  </p>
+                </>
+                : <>
+                  <p className="text-sm">
+                    Name: <strong>{character.name}</strong>
+                  </p>
+                  <button
+                    className="flex items-center gap-4 mt-2 cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
+                    type="button"
+                    onClick={() => { setChangeNickname(!changeNickname) }}
+                  >
+                    <PencilIcon className="icon max-w-6 max-h-6" /> Change nickname
+                  </button>
+                  <p className="text-sm mt-8">
+                    Map: <strong>{character.map.charAt(0).toUpperCase() + character.map.slice(1).toLowerCase()}</strong>
+                  </p>
+                  <button
+                    className="flex items-center gap-4 mt-2 cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
+                    type="button"
+                    onClick={() => { setResetPosition(!resetPosition) }}
+                  >
+                    <MapPinIcon className="icon max-w-6 max-h-6" /> Reset position
+                  </button>
+                  <p className="text-sm mt-8">
+                    User ID: <strong>{character.user.id}</strong>
+                  </p>
+                  <button
+                    className="flex items-center gap-4 mt-2 cursor-pointer underline duration-[.25s] hover:text-(--primary-orange-1)"
+                    type="button"
+                    onClick={() => { setTransferCharacter(!transferCharacter) }}
+                  >
+                    <DocumentArrowUpIcon className="icon max-w-6 max-h-6" /> Transfer character
+                  </button>
+                </>
+            )
           : <>
             <p>Created at: <strong>{new Date(character.created_at).toLocaleDateString()}</strong></p>
             <p>Name: <strong>{character.name}</strong></p>
             <p>Class: <strong>{character.class.charAt(0).toUpperCase() + character.class.slice(1).toLowerCase()}</strong></p>
             <p>Level: <strong>{character.level}</strong></p>
             <p>Map: <strong>{character.map.charAt(0).toUpperCase() + character.map.slice(1).toLowerCase()}</strong></p>
-            <p>Exp: <strong>{character.exp}</strong></p>
-            <p>Gold: <strong>{character.gold}</strong></p>
-            <p>Energy: <strong>{character.attributes.energy}</strong></p>
-            <p>Agility: <strong>{character.attributes.agility}</strong></p>
-            <p>Accuracy: <strong>{character.attributes.accuracy}</strong></p>
-            <p>Luck: <strong>{character.attributes.luck}</strong></p>
             <p>Status: <strong>{character.online_status ? 'Online' : 'Offline'}</strong></p>
             <p>Online time: <strong>{character.online_time}</strong></p>
-            <p>Last connection date: <strong>{new Date(character.last_connection_date).toLocaleDateString()}</strong></p>
+            <p>Last connection: <strong>{new Date(character.last_connection_date).toLocaleDateString()}</strong></p>
             <p>Last connection IP: <strong>{character.last_connection_ip}</strong></p>
           </>
         }
