fdb76f7e5b2c65175ef8a40e75bdd0c7a6e6aa72
Guilherme Neumann <guilherme.neumann@vnda.com.br>
2025-09-01 17:44:32 -0300

    Quickfix shop page text

--- frontend/src/app/shop/page.tsx
+++ frontend/src/app/shop/page.tsx
@@ -19,9 +19,9 @@ export default function ShopPage() {
           </p>
 
           <p className="text-base text-white w-full mt-6">
-            But you can still help us with a donate. <br/>
+            But you can still help us with a donate! <br/>
             All donates goes to game server host payment. <br/>
-            You can see monthly achieve progress below.
+            You can see monthly achieve progress below:
           </p>
 
           <div className="progress-bar flex flex-col items-center w-full mt-12">
@@ -30,12 +30,12 @@ export default function ShopPage() {
               <div className={`progress absolute top-0 left-0 min-w-[1%] h-[4px] rounded-[1.6rem] bg-(--primary-orange-1)`}></div>
             </div>
 
-            <p className="text text-xs text-white mt-4">
+            <p className="text text-sm text-white mt-4">
               R$ 1,00 / <strong>R$ 100,00</strong>
             </p>
           </div>
 
-          <strong className="text-base text-white w-full mt-12">
+          <strong className="text-base text-white w-full mt-8">
             Server will stay online even without a fully charged bar. <span className="font-normal line-through text-(--gray-3)">fow now</span> <br/>
             When you donate you are just helping the project.
           </strong>
