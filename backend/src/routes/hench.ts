dd9cbb0f0371643262f1cffe03e878c254f2658f
Guilherme Neumann <ae81ff@gmail.com>
2025-11-14 16:07:24 -0300

    Remove backend hench API get conditions

--- backend/src/routes/hench.ts
+++ backend/src/routes/hench.ts
@@ -3,14 +3,7 @@ import { myServerPrisma } from '../db.js'
 
 export async function henchRoutes(app: FastifyInstance) {
   app.get('/hench', async () => {
-    const list = await myServerPrisma.s_monster.findMany({
-      where: {
-        OR: [
-          { start_base_level: { gt: 250 } },
-          { name: { contains: 'CyberHare' } }
-        ]
-      }
-    })
+    const list = await myServerPrisma.s_monster.findMany()
     return list
   })
 }
