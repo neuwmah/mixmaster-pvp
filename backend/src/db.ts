6bc754da7f9b9f50e942b4ad0167ebf86a965560
Guilherme Neumann <ae81ff@gmail.com>
2025-11-18 09:29:44 -0300

    Add backend web account database

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,8 @@
-import { PrismaClient as PgPrismaClient } from '../prisma/generated/postgresql/index.js'
-import { PrismaClient as MyGamePrismaClient } from '../prisma/generated/mysql/game/index.js'
-import { PrismaClient as MyMemberPrismaClient } from '../prisma/generated/mysql/member/index.js'
-import { PrismaClient as MyServerPrismaClient } from '../prisma/generated/mysql/server/index.js'
+import { PrismaClient as PgPrismaClient } from '../prisma/_generated/postgresql/index.js'
+import { PrismaClient as MyGamePrismaClient } from '../prisma/_generated/mysql/game/index.js'
+import { PrismaClient as MyMemberPrismaClient } from '../prisma/_generated/mysql/member/index.js'
+import { PrismaClient as MyServerPrismaClient } from '../prisma/_generated/mysql/server/index.js'
+import { PrismaClient as MyWebPrismaClient } from '../prisma/_generated/mysql/web/index.js'
 
 declare global {
   // eslint-disable-next-line no-var
@@ -12,12 +13,15 @@ declare global {
   var myMemberPrisma: MyMemberPrismaClient | undefined
   // eslint-disable-next-line no-var
   var myServerPrisma: MyServerPrismaClient | undefined
+  // eslint-disable-next-line no-var
+  var myWebPrisma: MyWebPrismaClient | undefined
 }
 
 export const pgPrisma = global.pgPrisma || new PgPrismaClient()
 export const myGamePrisma = global.myGamePrisma || new MyGamePrismaClient()
 export const myMemberPrisma = global.myMemberPrisma || new MyMemberPrismaClient()
 export const myServerPrisma = global.myServerPrisma || new MyServerPrismaClient()
+export const myWebPrisma = global.myWebPrisma || new MyWebPrismaClient()
 export const prisma = pgPrisma
 
 if (process.env.NODE_ENV !== 'production') {
@@ -25,4 +29,5 @@ if (process.env.NODE_ENV !== 'production') {
   global.myGamePrisma = myGamePrisma
   global.myMemberPrisma = myMemberPrisma
   global.myServerPrisma = myServerPrisma
+  global.myWebPrisma = myWebPrisma
 }
